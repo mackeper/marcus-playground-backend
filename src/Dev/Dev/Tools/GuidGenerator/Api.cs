471a816d97157260fdabafbab0174bc22b196825
Marcus Östling <mpt.ostling@gmail.com>
2023-08-31 06:31:09 +0200

    Add tests to guid generator

--- src/Dev/Dev/Tools/GuidGenerator/Api.cs
+++ src/Dev/Dev/Tools/GuidGenerator/Api.cs
@@ -4,11 +4,15 @@ public class Api
 {
     public void RegisterEndpoints(WebApplication app)
     {
-        app.MapGet("/guid", () =>
-        {
-            return Guid.NewGuid().ToString();
-        })
-        .WithName("GetGuid")
-        .WithOpenApi();
+        app.MapGet("/guid", () => Guids(1).First());
+        app.MapGet("/guid/{count:int}", Guids);
     }
+
+    private Guid[] Guids(int count)
+    {
+        if (count < 0)
+            return Array.Empty<Guid>();
+        return Enumerable.Range(0, count).Select(_ => Guid.NewGuid()).ToArray();
+    }
+
 }
