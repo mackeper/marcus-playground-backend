ebae0b9b38368bc86e3a3453f6773689129e300d
Marcus Östling <mpt.ostling@gmail.com>
2023-08-31 22:26:33 +0200

    Rename output binary and add some functions to publish.ps1

--- src/Tracker/helloworld/publish.ps1
+++ src/Tracker/helloworld/publish.ps1
@@ -8,33 +8,55 @@
     3. Run the script using a PowerShell environment.
 #>
 
-function Copy-FilesToDirectory {
-    param (
-        [string]$sourceDirectory,
-        [string]$destinationDirectory,
-        [string[]]$fileList
-    )
-
+function Copy-FilesToDirectory([string]$sourceDirectory, [string]$destinationDirectory, [string[]]$fileList) {
     foreach ($file in $fileList) {
         $sourcePath = Join-Path $sourceDirectory $file
-        Copy-Item -Path $sourcePath -Destination $destinationDirectory -Recurse -Force
+        $destination = Join-Path $destinationDirectory $file
+        Copy-Item -Path $sourcePath -Destination $destination -Recurse -Force
     }
 }
 
-function Clear-Directory { param ([string]$directory)
-    Remove-Item -Path $directory\* -Recurse -Force
+function Clear-Directory([string]$directory) {
+    Remove-Item -Path $directory -Recurse -Force
 }
 
-function New-Directory { param ([string]$directory)
+function New-Directory([string]$directory) {
     if (-not (Test-Path -Path $directory)) {
         New-Item -Path $directory -ItemType Directory
     }
+}
+
+function CheckAndCreateDockerImage ([string]$imageName, [string]$imageTag, [string]$dockerfilePath = ".") {
+    if (docker images "${imageName}:${imageTag}" -q) {
+        return;
+    }
+
+    Write-Host "Docker image ${imageName}:${imageTag} does not exist. Building..."
+}
+function StartDockerContainerIfNotRunning([string]$containerName, [string]$imageName, [string]$imageTag) {
+    $containerStatus = docker ps -a --filter "name=$containerName" --format "{{.Status}}"
+
+    if ($containerStatus -like "Up*") {
+        Write-Host "Container $containerName is already running."
+    } else {
+        Write-Host "Container $containerName is not running. Starting..."
+        docker run --name $containerName -v "$(Get-Location)\docker-volume:/docker-volume" -dit "${imageName}:${imageTag}"
+    }
+}
+
+function RunStackBuildInDockerContainer([string]$containerName, [string]$workDir) {
+
+    docker exec --workdir $workDir $containerName /bin/bash "-c" "stack build"
 }
 
 function Main {
     # Define source and destination directories
     $sourceDir = "."
     $destinationDir = ".\docker-volume"
+    $outDir = ".\out"
+    $imageName = "debian11-haskell"
+    $imageTag = "v1.0"
+    $containerName = "debian11-haskell"
 
     # Define the list of files to copy, relative to the source directory
     $fileList = @(
@@ -51,9 +73,13 @@ function Main {
         "CHANGELOG.md"
     )
 
-    New-Directory -directory $destinationDir
     Clear-Directory -directory $destinationDir
+    New-Directory -directory $destinationDir
     Copy-FilesToDirectory -sourceDirectory $sourceDir -destinationDirectory $destinationDir -fileList $fileList
+    CheckAndCreateDockerImage -imageName $imageName -imageTag $imageTag -dockerfilePath $destinationDir
+    StartDockerContainerIfNotRunning -containerName $containerName -imageName $imageName -imageTag $imageTag
+    RunStackBuildInDockerContainer -containerName $containerName -workDir "/docker-volume"
+    New-Directory -directory $outDir
 }
 
 Main
