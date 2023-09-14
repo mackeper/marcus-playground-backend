7991356a64aef33648cf48797e5becae5a3dead3
Marcus Östling <mpt.ostling@gmail.com>
2023-09-14 21:59:24 +0200

    remove adding a Visit in setupDatabase

--- src/Tracker/app/Main.hs
+++ src/Tracker/app/Main.hs
@@ -2,25 +2,12 @@ module Main (main) where
 
 import Lib (startApp)
 import Visits.Persistence (
-    getVisitCount,
-    incrementVisitCount,
     migrate,
  )
-
-import Data.Time
-import Visits.Visit (createVisit)
 
 setupDatabase :: String -> IO ()
 setupDatabase path = do
     migrate path
-
-    visit1 <- getVisitCount path
-    putStrLn $ "Current visit count: " ++ show visit1
-
-    _ <- incrementVisitCount path (createVisit "https://realmoneycompany.com" UTCTime{utctDay = fromGregorian 2023 1 1, utctDayTime = timeOfDayToTime (TimeOfDay 0 0 0)})
-
-    visit2 <- getVisitCount path
-    putStrLn $ "Current visit count: " ++ show visit2
 
 main :: IO ()
 main = do
