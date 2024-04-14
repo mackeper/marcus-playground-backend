83cd836e059092c6782fb19d7153f4e4bee4874e
Marcus Östling <mpt.ostling@gmail.com>
2024-04-14 08:46:24 +0200

    Revert "Revert "Start with an identity server""
    This reverts commit 3a0933be21fcaee3d2f43ba7a89a937bea981314.

---
+++ src/Identity/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity;
+
+internal class Program {
+    private static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("AppDb"));
+
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment()) {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        /* app.UseHttpsRedirection(); */
+
+        var summaries = new[]
+        {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+        app.MapGet("/weatherforecast", () => {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast")
+        .WithOpenApi()
+        .RequireAuthorization();
+
+        app.MapIdentityApi<IdentityUser>();
+
+        app.Run();
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options) { }
