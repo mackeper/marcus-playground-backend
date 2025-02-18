27b33ca98d909c2220e9396ebb7952d691e70c17
Marcus Östling <mpt.ostling@gmail.com>
2025-02-18 21:49:35 +0100

    Update blog to net8

--- src/Blog/BlogService/publish.ps1
+++ src/Blog/BlogService/publish.ps1
@@ -1,4 +1,4 @@
 dotnet publish -c Release -r linux-x64
 # dotnet ef migrations bundle --self-contained -r linux-x64
-# Move-Item -Force efbundle 'bin\Release\net7.0\linux-x64\publish\'
-scp -r C:\Users\macke\source\repos\JustABackend\src\Blog\BlogService\bin\Release\net7.0\linux-x64\publish\* gcp:~/Services/Blog/
+# Move-Item -Force efbundle 'bin\Release\net8.0\linux-x64\publish\'
+scp -r C:\Users\macke\source\repos\JustABackend\src\Blog\BlogService\bin\Release\net8.0\linux-x64\publish\* gcp:~/Services/Blog/
